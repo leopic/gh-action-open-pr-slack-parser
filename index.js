0cd05da5d9eaf02a84b7dfc0b659ee0f997efbc5
Leo Picado <leopic@gmail.com>
2020-05-04 16:55:30 -0600

    Attempt 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const context = require('@actions/github').context;
 const { Octokit } = require("@octokit/rest");
 
