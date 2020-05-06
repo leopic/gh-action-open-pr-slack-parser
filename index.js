f3acf7e73b682804f69ee589dafde7afe663b200
Leo Picado <leopic@gmail.com>
2020-05-06 14:58:26 -0600

    Test

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ const lib = require('./src/lib');
 
 module.exports.run = async () => {
   try {
-    const originalRepository = process.env['GITHUB_REPOSITORY'];
-
-    if (process.env['MOCK_GITHUB_REPOSITORY'] !== '') {
-      process.env['GITHUB_REPOSITORY'] = process.env['MOCK_GITHUB_REPOSITORY'];
-    }
-
     const {owner, repo} = context.repo;
     const token = process.env['GITHUB_TOKEN'] || '';
     const output = await lib.work({token, owner, repo});
-
     core.setOutput('message', output);
     core.info(output);
-
-    process.env['GITHUB_REPOSITORY'] = originalRepository;
   } catch (error) {
     core.setFailed(error.message)
   }
